6e418aec494b3138eb0621b2153c2409544d9cdf
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-02-13 13:10:07 +1030

    Update microshell.h

--- microshell.h
+++ microshell.h
@@ -1,7 +1,8 @@
 #ifndef MICROSHELL_H
 # define MICROSHELL_H
 
-# include <unistd.h>
-# include <libc.h>
+#include <string.h> // strcmp
+#include <unistd.h> // write, dup, dup2, close, execve, chdir and fork
+#include <sys/wait.h> // waitpid
 
 #endif
