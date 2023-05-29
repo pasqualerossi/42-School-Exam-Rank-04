4ebbd463179b4f6d7333cea49474333df29e7ec9
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-05-29 11:53:29 +0930

    Update microshell.h

--- microshell.h
+++ microshell.h
@@ -1,11 +1,11 @@
 #ifndef MICROSHELL_H
 # define MICROSHELL_H
 
-#include <string.h> // strcmp
-#include <unistd.h> // write, dup, dup2, close, execve, chdir and fork
-#include <sys/wait.h> // waitpid
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int write_error(char *string, char *argv);
-int	ft_exe(char **argv, int i, int temporary_file_descriptor, char **environment_variables);
+int ft_exe(char **argv, int i, int temporary_file_descriptor, char **environment_variables);
 
 #endif
