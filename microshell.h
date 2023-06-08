98a8c02d9d05aab3e1df829cb493815c373a53bf
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-06-08 19:42:26 +0930

    Update microshell.h

--- microshell.h
+++ microshell.h
@@ -5,7 +5,4 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int write_error(char *string, char *argv);
-int ft_exe(char **argv, int i, int temporary_file_descriptor, char **environment_variables);
-
 #endif
