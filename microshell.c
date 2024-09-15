5f46e432da74e875b54d78b2788084b0a98133f4
Pasquale Rossi <58959408+pasqualerossi@users.noreply.github.com>
2024-09-16 05:46:17 +0930

    Update microshell.c

--- microshell.c
+++ microshell.c
@@ -27,15 +27,12 @@ void set_pipe(int has_pipe, int *fd, int end)
 int	exec(char **argv, int i, char **envp)
 {
 	int has_pipe, fd[2], pid, status;
-
 	has_pipe = argv[i] && !strcmp(argv[i], "|");
 
 	if (!has_pipe && !strcmp(*argv, "cd"))
 		return cd(argv, i);
-
 	if (has_pipe && pipe(fd) == -1)
 		err("error: fatal\n"), exit(1);
-
 	if ((pid = fork()) == -1)
 		err("error: fatal\n"), exit(1);
 	if (!pid)
